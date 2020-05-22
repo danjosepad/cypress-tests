81c12ce613e577774d4f27104ea4003db7b4fba3
danjosepad <danielpadilhapessoal@outlook.com>
2020-05-22 17:57:14 -0300

    chore: xpath plugin added

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,14 +1,13 @@
 /// <reference types="cypress" />
 
 describe('Work with basic elements', () => {
-  it('text', () => {]
+  it('text', () => {
     cy.visit('https://www.wcaquino.me/cypress/componentes.html')
 
-    cy.get('body')
-      .should('contain', 'Cuidado')
-
+    cy.get('body').should('contain', 'Cuidado')     
     // Mais restritivo
-    cy.get('span')
-      .should('contain', 'Cuidado')
+    cy.get('span').should('contain', 'Cuidado')     
+    cy.get('.facilAchar')
+      .should('have.text', 'Cuidado onde clica, muitas armadilhas...')
   })
 })
