a95f2a1b935917bb2920b09b0800ab433e5b18a1
danjosepad <danielpadilhapessoal@outlook.com>
2020-05-22 18:06:54 -0300

    feat: text using links added

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,13 +1,22 @@
 /// <reference types="cypress" />
 
 describe('Work with basic elements', () => {
-  it('text', () => {
-    cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+  beforeEach(() => {
+    cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+  })
 
+  it('text', () => {
     cy.get('body').should('contain', 'Cuidado')     
     // Mais restritivo
     cy.get('span').should('contain', 'Cuidado')     
     cy.get('.facilAchar')
       .should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+  })
+
+  it('links', () => {
+    cy.get('#resultado').should('have.not.text', 'Voltou!');
+
+    cy.contains('Voltar').click();
+    cy.get('#resultado').should('have.text', 'Voltou!');
   })
 })
